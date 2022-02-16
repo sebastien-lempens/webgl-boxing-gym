854adc0f16c5a1ba07b6e1735e77b0dcd854dda9
telkeatelecom <telkeatelecom@gmail.com>
2022-02-16 20:50:44 +0100

    fix@ lights

--- assets/js/App.js
+++ assets/js/App.js
@@ -3,184 +3,168 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
 
 export default class App {
-  #resizeCallback = () => this.#onResize()
-  #mouseMoveCallback = (e) => this.#onMouseMove(e)
+	#resizeCallback = () => this.#onResize()
+	#mouseMoveCallback = (e) => this.#onMouseMove(e)
 
-  constructor(container) {
-    this.container = document.querySelector(container)
-  }
+	constructor(container) {
+		this.container = document.querySelector(container)
+	}
 
-  async init() {
-    window.__APP__ = this
-    this.#createParams()
-    this.#createScene()
-    this.#createCamera()
-    this.#createRenderer()
-    this.#createLight()
-    this.#createClock()
-    this.#addListeners()
-    this.#createControls()
-    this.#createDebugPanel()
-    this.#createLoaders()
-    await this.#loadModel()
-    this.renderer.setAnimationLoop(() => {
-      this.#update()
-      this.#render()
-    })
-  }
-  destroy() {
-    this.renderer.dispose()
-    this.#removeListeners()
-  }
+	async init() {
+		window.__APP__ = this
+		this.#createParams()
+		this.#createScene()
+		this.#createCamera()
+		this.#createRenderer()
+		this.#createLight()
+		this.#createClock()
+		this.#addListeners()
+		this.#createControls()
+		this.#createDebugPanel()
+		this.#createLoaders()
+		await this.#loadModel()
+		this.renderer.setAnimationLoop(() => {
+			this.#update()
+			this.#render()
+		})
+	}
+	destroy() {
+		this.renderer.dispose()
+		this.#removeListeners()
+	}
 
-  #update() {
-    const elapsed = this.clock.getElapsedTime()
-    this.controls.update()
-    if (this.params.onTickModel.length > 0) {
-      this.params.onTickModel.forEach((model) => {
-        if (typeof model.onTick === 'function') {
-          return model.onTick(elapsed)
-        }
-      })
-    }
-  }
+	#update() {
+		const elapsed = this.clock.getElapsedTime()
+		this.controls.update()
+		if (this.params.onTickModel.length > 0) {
+			this.params.onTickModel.forEach((model) => {
+				if (typeof model.onTick === 'function') {
+					return model.onTick(elapsed)
+				}
+			})
+		}
+	}
 
-  #render() {
-    this.renderer.render(this.scene, this.camera)
-  }
-  #createParams() {
-    this.params = {
-      onTickModel: [],
-      mouse: new THREE.Vector2(),
-    }
-  }
-  #createScene() {
-    this.scene = new THREE.Scene()
-  }
-  #createCamera() {
-    this.camera = new THREE.PerspectiveCamera(
-      75,
-      this.container.clientWidth / this.container.clientHeight,
-      0.01,
-      1000
-    )
-    this.camera.position.set(-1, 1, 2)
-  }
-  #createRenderer() {
-    this.renderer = new THREE.WebGLRenderer({
-      alpha: true,
-      antialias: window.devicePixelRatio === 1,
-    })
+	#render() {
+		this.renderer.render(this.scene, this.camera)
+	}
+	#createParams() {
+		this.params = {
+			onTickModel: [],
+			mouse: new THREE.Vector2(),
+		}
+	}
+	#createScene() {
+		this.scene = new THREE.Scene()
+	}
+	#createCamera() {
+		this.camera = new THREE.PerspectiveCamera(
+			45,
+			this.container.clientWidth / this.container.clientHeight,
+			0.01,
+			200
+		)
+		this.camera.position.set(-2, 2, 4)
+	}
+	#createRenderer() {
+		this.renderer = new THREE.WebGLRenderer({
+			alpha: true,
+			antialias: window.devicePixelRatio === 1,
+		})
 
-    this.container.appendChild(this.renderer.domElement)
+		this.container.appendChild(this.renderer.domElement)
 
-    this.renderer.setSize(
-      this.container.clientWidth,
-      this.container.clientHeight
-    )
-    this.renderer.setPixelRatio(Math.min(1.5, window.devicePixelRatio))
-    this.renderer.setClearColor(0x121212)
-    this.renderer.physicallyCorrectLights = true
-  }
-  #createLight() {
-    const light = new THREE.AmbientLight(0xffffff)
-    light.intensity = 1.5
-    this.scene.add(light)
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 5)
-    directionalLight.position.x = -5
-    directionalLight.position.z = 5
-    this.scene.add(directionalLight)
+		this.renderer.setSize(
+			this.container.clientWidth,
+			this.container.clientHeight
+		)
+		this.renderer.setPixelRatio(Math.min(1.5, window.devicePixelRatio))
+		this.renderer.setClearColor(0x121212)
+		this.renderer.physicallyCorrectLights = true
+	}
+	#createLight() {
+		const light = new THREE.AmbientLight(0xffffff)
+		light.intensity = 1.5
+		this.scene.add(light)
+		const directionalLight = new THREE.DirectionalLight(0xffffff, 5)
+		directionalLight.position.x = -5
+		directionalLight.position.z = 5
+		this.scene.add(directionalLight)
 
-    {
-      const width = 2.5
-      const height = 1
-      const intensity = 1
-      const rectLight = new THREE.RectAreaLight(
-        0xffffff,
-        intensity,
-        width,
-        height
-      )
-      rectLight.position.set(0.85,2,-1.5)
-      //rectLight.lookAt(0, 0, 0)
-      this.scene.add(rectLight)
-	  const rectLightHelper = new RectAreaLightHelper( rectLight );
-	  rectLight.add( rectLightHelper );
-    }
-  }
-  #createLoaders() {
-    this.loadingManager = new THREE.LoadingManager()
+	}
+	#createLoaders() {
+		this.loadingManager = new THREE.LoadingManager()
 
-    this.loadingManager.onProgress = (url, loaded, total) => {
-      // In case the progress count is not correct, see this:
-      // https://discourse.threejs.org/t/gltf-file-loaded-twice-when-loading-is-initiated-in-loadingmanager-inside-onprogress-callback/27799/2
-      console.log(`Loaded ${loaded} resources out of ${total} -> ${url}`)
-    }
+		this.loadingManager.onProgress = (url, loaded, total) => {
+			// In case the progress count is not correct, see this:
+			// https://discourse.threejs.org/t/gltf-file-loaded-twice-when-loading-is-initiated-in-loadingmanager-inside-onprogress-callback/27799/2
+		//	console.log(`Loaded ${loaded} resources out of ${total} -> ${url}`)
+		}
 
-    this.loadingManager.onLoad = () => {
-      console.log('All resources loaded')
-    }
+		this.loadingManager.onLoad = () => {
+			console.log('All resources loaded')
+		}
 
-    //	this.gltfLoader = new GLTFLoader(this.loadingManager)
-  }
-  /**
-   * Load Models and append them to the scene
-   */
-  async #loadModel() {
-    const loadModels = new (await import('./Models.js')).default()
-  }
-  #createControls() {
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-    this.controls.enableDamping = true
-  }
-  async #createDebugPanel() {
-    /**
-     * Load Tweakpane
-     */
-    const loadDebugPanel = new (await import('./DebugPanel.js')).default()
-    this.pane = loadDebugPanel.pane
-    /**
-     * Scene configuration
-     */
-    this.pane.addFolder({ title: 'Scene' })
-    this.pane
-      .addInput({ background: { r: 18, g: 18, b: 18 } }, 'background', {
-        label: 'Background Color',
-      })
-      .on('change', (e) => {
-        this.renderer.setClearColor(
-          new THREE.Color(e.value.r / 255, e.value.g / 255, e.value.b / 255)
-        )
-      })
-  }
-  #createClock() {
-    this.clock = new THREE.Clock()
-  }
-  #addListeners() {
-    window.addEventListener('resize', this.#resizeCallback, { passive: true })
-    window.addEventListener('mousemove', this.#mouseMoveCallback, {
-      passive: true,
-    })
-  }
-  #removeListeners() {
-    window.removeEventListener('resize', this.#resizeCallback, {
-      passive: true,
-    })
-    window.removeEventListener('mousemove', this.#resizeCallback, {
-      passive: true,
-    })
-  }
-  #onMouseMove({ clientX, clientY }) {
-    this.params.mouse.x = (clientX / this.container.clientWidth) * 2 - 1
-    this.params.mouse.y = (-clientY / this.container.clientHeight) * 2 + 1
-  }
-  #onResize() {
-    this.camera.aspect =
-      this.container.clientWidth / this.container.clientHeight
-    this.camera.updateProjectionMatrix()
-    this.renderer.setSize(
-      this.container.clientWidth,
-      this.container.clientHeight
-    )
-  }
+		//	this.gltfLoader = new GLTFLoader(this.loadingManager)
+	}
+	/**
+	 * Load Models and append them to the scene
+	 */
+	async #loadModel() {
+		const loadModels = new (await import('./Models.js')).default()
+	}
+	#createControls() {
+		this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+		this.controls.enableDamping = true
+	}
+	async #createDebugPanel() {
+		/**
+		 * Load Tweakpane
+		 */
+		const loadDebugPanel = new (await import('./DebugPanel.js')).default()
+		this.pane = loadDebugPanel.pane
+		/**
+		 * Scene configuration
+		 */
+		this.pane.addFolder({ title: 'Scene' })
+		this.pane
+			.addInput({ background: { r: 18, g: 18, b: 18 } }, 'background', {
+				label: 'Background Color',
+			})
+			.on('change', (e) => {
+				this.renderer.setClearColor(
+					new THREE.Color(e.value.r / 255, e.value.g / 255, e.value.b / 255)
+				)
+			})
+	}
+	#createClock() {
+		this.clock = new THREE.Clock()
+	}
+	#addListeners() {
+		window.addEventListener('resize', this.#resizeCallback, { passive: true })
+		window.addEventListener('mousemove', this.#mouseMoveCallback, {
+			passive: true,
+		})
+	}
+	#removeListeners() {
+		window.removeEventListener('resize', this.#resizeCallback, {
+			passive: true,
+		})
+		window.removeEventListener('mousemove', this.#resizeCallback, {
+			passive: true,
+		})
+	}
+	#onMouseMove({ clientX, clientY }) {
+		this.params.mouse.x = (clientX / this.container.clientWidth) * 2 - 1
+		this.params.mouse.y = (-clientY / this.container.clientHeight) * 2 + 1
+	}
+	#onResize() {
+		this.camera.aspect =
+			this.container.clientWidth / this.container.clientHeight
+		this.camera.updateProjectionMatrix()
+		this.renderer.setSize(
+			this.container.clientWidth,
+			this.container.clientHeight
+		)
+	}
 }
--- assets/js/Models.js
+++ assets/js/Models.js
@@ -5,124 +5,157 @@ import vertexShader from '/assets/shaders/vertex.glsl'
 import fragmentShader from '/assets/shaders/fragment.glsl'
 
 export default class Models {
-  constructor() {
-    this.app = window.__APP__
-    this.init()
-  }
-  async init() {
-    await this.#loadGltfModel()
-    //  await this.#createShadedBox()
-  }
-  async #loadGltfModel() {
-    const group = new THREE.Group()
-    const dracoLoader = new DRACOLoader()
-    dracoLoader.setDecoderPath('/assets/draco/')
-    const gltfLoader = new GLTFLoader(this.app.loadingManager)
-    gltfLoader.setDRACOLoader(dracoLoader)
-    const meshes = await new Promise((resolve) => {
-      gltfLoader.load('/assets/mesh/BoxingGym-draco.gltf', async (scene) => {
-        const children = scene.scene.children[0].children
-        await Promise.all(
-          children.map(async (child) => {
-            const { name } = child
-            await new Promise((resolve) => {
-              new THREE.TextureLoader(this.app.loadingManager).load(
-                `/assets/texture/Diffuse/${name}.webp`,
-                (texture) => {
-                  if (texture) {
-                    texture.flipY = false
-                    if (name === 'WindowLight') {
-                      child.material.side = THREE.DoubleSide
-                      child.material.emissive = new THREE.Color(0xffffff)
-                    }
-                    child.material.metalness = 0
-                    child.material.roughness = 1
-                    child.material.map = texture
-                    texture.needsUpdate = true
-                  }
-                  resolve()
-                }
-              )
-            })
-            await new Promise((resolve) => {
-              new THREE.TextureLoader(this.app.loadingManager).load(
-                `/assets/texture/Roughness/${name}.webp`,
-                (texture) => {
-                  if (texture) {
-                    texture.flipY = false
-                    child.material.roughnessMap = texture
-                    texture.needsUpdate = true
-                  }
-                  resolve()
-                }
-              )
-            })
-            await new Promise((resolve) => {
-              new THREE.TextureLoader(this.app.loadingManager).load(
-                `/assets/texture/Normal/${name}.webp`,
-                (texture) => {
-                  if (texture) {
-                    texture.flipY = false
-                    child.material.normalScale = new THREE.Vector2(0.5, 0.5)
-                  //  child.material.normalMapType = THREE.ObjectSpaceNormalMap
-                    child.material.normalMap = texture
-                    texture.needsUpdate = true
-                  }
-                  resolve()
-                }
-              )
-            })
-          })
-        )
-        resolve(children)
-      })
-    })
-    group.add(...meshes)
-    this.app.scene.add(group)
-  }
-  async #createShadedBox() {
-    /**
-     * Create a box with a custom ShaderMaterial
-     */
-    const geometry = new THREE.BoxGeometry(1, 1, 1, 1, 1, 1)
-    const texture = await new Promise((resolve) => {
-      new THREE.TextureLoader(this.app.loadingManager).load(
-        '/assets/texture/uvchecker.png',
-        (texture) => {
-          resolve(texture)
-        }
-      )
-    })
-    const material = new THREE.ShaderMaterial({
-      uniforms: {
-        uResolution: {
-          value: new THREE.Vector2(
-            this.app.container.clientWidth,
-            this.app.container.clientHeight
-          ),
-        },
-        uTexture: {
-          value: texture,
-        },
-        uTime: {
-          value: null,
-        },
-        uMouse: {
-          value: this.app.params.mouse,
-        },
-      },
-      vertexShader,
-      fragmentShader,
-      transparent: true,
-    })
-    this.shadedBox = new THREE.Mesh(geometry, material)
-    this.shadedBox.name = 'shadedBox'
-    this.app.params.onTickModel.push(this.shadedBox)
-    //this.app.scene.add(this.shadedBox)
+	constructor() {
+		this.app = window.__APP__
+		this.init()
+	}
+	async init() {
+		await this.#loadGltfModel()
+		//  await this.#createShadedBox()
+	}
+	async #loadGltfModel() {
+		const group = new THREE.Group()
+		const dracoLoader = new DRACOLoader()
+		dracoLoader.setDecoderPath('/assets/draco/')
+		const gltfLoader = new GLTFLoader(this.app.loadingManager)
+		gltfLoader.setDRACOLoader(dracoLoader)
+		const meshes = await new Promise(async (resolve) => {
+			gltfLoader.load('/assets/mesh/BoxingGym-draco.gltf', async (scene) => {
+				const children = scene.scene.children[0].children
+				await Promise.all(
+					children.map(async (child) => {
+						const { name } = child
+						await new Promise((resolve) => {
+							new THREE.TextureLoader(this.app.loadingManager).load(
+								`/assets/texture/Diffuse/${name}.webp`,
+								(texture) => {
+									if (texture) {
+										texture.flipY = false
+										if (name === 'WindowLight') {
+											child.material.side = THREE.DoubleSide
+											child.material.emissive = new THREE.Color(0xffffff)
+										}
+										child.material.metalness = 0
+										child.material.roughness = 1
+										child.material.map = texture
+									}
+									resolve()
+								}
+							)
+						})
+						await new Promise((resolve) => {
+							new THREE.TextureLoader(this.app.loadingManager).load(
+								`/assets/texture/Roughness/${name}.webp`,
+								(texture) => {
+									if (texture) {
+										texture.flipY = false
+										child.material.roughnessMap = texture
+									}
+									resolve()
+								}
+							)
+						})
+						await new Promise((resolve) => {
+							new THREE.TextureLoader(this.app.loadingManager).load(
+								`/assets/texture/Normal/${name}.webp`,
+								(texture) => {
+									if (texture) {
+										texture.flipY = false
+										child.material.normalScale = new THREE.Vector2(0.5, 0.5)
+										child.material.normalMap = texture
+									}
+									resolve()
+								}
+							)
+						})
+					})
+				)
+				resolve(children)
+			})
+		})
+		group.add(...meshes)
+		this.app.scene.add(group)
+		return
+		group.children.forEach((child) => {
+			if (child.getObjectByName('Carpet')) {
+				const material = child.getObjectByName('Carpet').material
+				const t = { value: 100 }
+				material.onBeforeCompile = (shader) => {
+					console.log(shader)
+					shader.uniforms.metalness = t
+					material.shader = shader
+					shader.vertexShader = `
+          varying vec3 vPosition; 
+          ${shader.vertexShader}`
+					shader.vertexShader = shader.vertexShader.replace(
+						/#include <begin_vertex>/,
+						(match) =>
+							`
+            ${match}
+            vPosition = position;
+            `
+					)
+					shader.fragmentShader = `
+          varying vec3 vPosition; 
+          ${shader.fragmentShader}`
+					shader.fragmentShader = shader.fragmentShader.replace(
+						/#include <dithering_fragment>/,
+						(match) =>
+							`
+            ${match}
+            vec3 lightDirection = normalize(vPosition - vec3(0.25,0.25,0.0));
+            float light = max(dot(-lightDirection * cameraPosition, vNormal),0.0);
+            vec3 updateFragColor = outgoingLight.rgb  + light * 0.3;
+            gl_FragColor = vec4(vec3(updateFragColor), 1.0);
+            `
+					)
+				}
+			}
+		})
+	}
+	async #createShadedBox() {
+		/**
+		 * Create a box with a custom ShaderMaterial
+		 */
+		const geometry = new THREE.BoxGeometry(1, 1, 1, 1, 1, 1)
+		const texture = await new Promise((resolve) => {
+			new THREE.TextureLoader(this.app.loadingManager).load(
+				'/assets/texture/uvchecker.png',
+				(texture) => {
+					resolve(texture)
+				}
+			)
+		})
+		const material = new THREE.ShaderMaterial({
+			uniforms: {
+				uResolution: {
+					value: new THREE.Vector2(
+						this.app.container.clientWidth,
+						this.app.container.clientHeight
+					),
+				},
+				uTexture: {
+					value: texture,
+				},
+				uTime: {
+					value: null,
+				},
+				uMouse: {
+					value: this.app.params.mouse,
+				},
+			},
+			vertexShader,
+			fragmentShader,
+			transparent: true,
+		})
+		this.shadedBox = new THREE.Mesh(geometry, material)
+		this.shadedBox.name = 'shadedBox'
+		this.app.params.onTickModel.push(this.shadedBox)
+		//this.app.scene.add(this.shadedBox)
 
-    /** OnTick() */
-    this.shadedBox.onTick = (elapsed) => {
-      material.uniforms.uTime.value = elapsed
-    }
-  }
+		/** OnTick() */
+		this.shadedBox.onTick = (elapsed) => {
+			material.uniforms.uTime.value = elapsed
+		}
+	}
 }
